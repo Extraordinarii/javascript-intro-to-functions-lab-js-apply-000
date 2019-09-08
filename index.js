13654da34f3162300e8010e1de2db0cffd45ec66
exuberant-pool-9484 <alisherfayzimatov@gmail.com>
2019-09-08 23:48:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function whisper(arg){
 function logShout(arg){
   console.log(arg.toUpperCase())
 }
+function logWhisper(arg){
+  console.log(arg.toLowerCase())
+}
+function sayHiToGrandma(arg){
+  if(arg==="hello"){
+    return "I can't hear you"
+  }else if(arg==="HELLO"){
+    return "YES INDEED!"
+  }else{
+    return "I love you, too"
+  }
+}
